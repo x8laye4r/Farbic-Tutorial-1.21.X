c80d1f359870db8519d983716ed5ee0fa7e4c14a
x8laye4r <eiseltdaniel5@gmial.com>
2024-12-07 18:51:53 +0100

    added important comment

---
+++ src/main/java/net/x8laye4r/tutorialmod/TutorialMod.java
@@ -0,0 +1,17 @@
+package net.x8laye4r.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/x8laye4r/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.x8laye4r.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
